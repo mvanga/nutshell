9d0e4bd7f6b7126cf1f9ba7fdb3aefdc12c54c70
Manohar Vanga <manohar.vanga@gmail.com>
2011-04-27 10:23:46 +0200

    Added help command
    Signed-off-by: Manohar Vanga <manohar.vanga@gmail.com>

--- modules/core/core.c
+++ modules/core/core.c
@@ -5,6 +5,18 @@
 
 NUT_NAME("core")
 
+int core_help(int argc, char *argv[])
+{
+	struct shell_cmd *c;
+	c = cmd_list;
+	printf("Available commands:\n");
+	while (c) {
+		printf("    %-20s%s\n", c->name, c->desc);
+		c = c->next;
+	}
+	return 0;
+}
+
 int core_quit(int argc, char *argv[])
 {
 	printf("Quit!\n");
@@ -14,6 +26,7 @@ int core_quit(int argc, char *argv[])
 int core_init()
 {
 	register_command("quit", 'q', "Quit", &core_quit);
+	register_command("help", 'h', "Help", &core_help);
 	return 0;
 }
 
